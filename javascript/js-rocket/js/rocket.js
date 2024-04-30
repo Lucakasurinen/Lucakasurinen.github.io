4ef3f75d9019c44687fe1477dda80ef6aa28abd8
Lucakasurinen <luca.kasurinen9@gmail.com>
2024-04-30 13:27:36 +0300

    rocket

--- javascript/js-rocket/js/rocket.js
+++ javascript/js-rocket/js/rocket.js
@@ -4,11 +4,15 @@ var countdownNumber = 10;
 var changeState = function (state){
     document.body.className = 'body-state'+ state;
 clearInterval(timer);
+countdownNumber = 10;
+document.getElementById('countdown').innerHTML = countdownNumber;
+
 if (state == 2) {
     timer = setInterval(function () {
-        document.getElementById('countdown').innerHTML = countdownNumber;
         countdownNumber = countdownNumber
          -1;
+        document.getElementById('countdown').innerHTML = countdownNumber;
+        
         if (countdownNumber <=0){
             changeState(3);
         }
@@ -18,14 +22,16 @@ if (state == 2) {
     var success = setTimeout(function() {
             var randomNumber = Math.round(Math.random()*10);
 
+            console.log('randomNumber;', randomNumber)
+
             //success
             if (randomNumber > 5) {
                 changeState(4);
-                
+                //do something
 
             } else {
                 changeState(5); //oh no!
-                
+                //do something else
             }
         }, 2000);
     }
