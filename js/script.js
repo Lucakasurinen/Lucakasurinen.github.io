94e5cb7941cc209f216b46b644512a2e2e93de6a
Lucakasurinen <luca.kasurinen9@gmail.com>
2024-05-29 23:42:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+//navigation bar
   function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
@@ -6,3 +6,5 @@
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+
